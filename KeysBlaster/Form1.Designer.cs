4bb5cfbbaa848b27a71dde077c2c5f41688347b4
Vishnu karthik L <vishnu_him@yahoo.co.in>
2012-10-21 10:38:58 +0530

    added Words per minute

--- KeysBlaster/Form1.Designer.cs
+++ KeysBlaster/Form1.Designer.cs
@@ -35,12 +35,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.secondsToWait = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.wpm = new System.Windows.Forms.NumericUpDown();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.secondsToWait)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wpm)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Location = new System.Drawing.Point(12, 59);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(260, 71);
@@ -52,7 +56,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 202);
+            this.button1.Location = new System.Drawing.Point(67, 253);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 48);
             this.button1.TabIndex = 1;
@@ -65,9 +69,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(153, 13);
+            this.label1.Size = new System.Drawing.Size(123, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "Enter the text to be sent rapidly";
+            this.label1.Text = "Enter the text to be sent ";
             // 
             // secondsToWait
             // 
@@ -85,11 +89,33 @@
             this.label2.TabIndex = 4;
             this.label2.Text = "Seconds to wait";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Words Per Minute";
+            // 
+            // wpm
+            // 
+            this.wpm.Location = new System.Drawing.Point(118, 209);
+            this.wpm.Name = "wpm";
+            this.wpm.Size = new System.Drawing.Size(47, 20);
+            this.wpm.TabIndex = 6;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(285, 357);
+            this.Controls.Add(this.wpm);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.secondsToWait);
             this.Controls.Add(this.label1);
@@ -99,6 +125,7 @@
             this.Text = "Key Blaster";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.secondsToWait)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wpm)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +139,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown secondsToWait;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown wpm;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- KeysBlaster/Form1.cs
+++ KeysBlaster/Form1.cs
@@ -11,7 +11,9 @@ namespace KeysBlaster
 {
     public partial class Form1 : Form
     {
+        int indexToSend = 0;
         String inputText = "";
+        String[] toSend;
         public Form1()
         {
             InitializeComponent();
@@ -19,34 +21,49 @@ namespace KeysBlaster
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            timer1.Enabled = true;
             refreshTimer();
         }
 
         private void refreshTimer()
         {
-            Decimal seconds = secondsToWait.Value;
+            int seconds = (int)secondsToWait.Value;
             seconds = seconds <= 0 ? 1 : seconds;
-            timer1.Interval = (int)seconds * 1000;
+            timer1.Interval = seconds * 1000;
+        }
+
+        private void refreshWpm()
+        {
+            int wordsPerMinute = (int)wpm.Value;
+            wordsPerMinute = wordsPerMinute <= 0 ? 60 : wordsPerMinute;
+            timer2.Interval = (int)((60.0 / wordsPerMinute) * 1000);
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            timer1.Enabled = true;
             refreshTimer();
+            refreshWpm();
             inputText = textBox1.Text;
+            toSend = inputText.Split(' ');
             timer1.Start();
-
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            String[] toSend=inputText.Split(' ');
-            foreach (String item in toSend)
-            {
-               
-                SendKeys.Send(item+" ");
-            }
+            timer2.Start();
             timer1.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (indexToSend == toSend.Length)
+            {
+                timer2.Stop();
+                indexToSend = 0;
+            }
+            else
+                SendKeys.Send(toSend[indexToSend++] + " ");
         }
 
     }
